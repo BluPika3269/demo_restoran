d03d90bfa22061dbbd42a9339a27a4504e4dbcfa
unknown <damjan.mitrovic.job@gmail.com>
2025-11-25 17:48:06 +0100

    Update deployment

--- deploy-all.bat
+++ deploy-all.bat
@@ -81,12 +81,8 @@ git push origin master
 echo.
 echo [4/4] Deploy na Vercel produkciju...
 cd client
-if exist .vercel (
-    echo Ciscenje starog Vercel linka...
-    rmdir /s /q .vercel
-)
 echo Pokrecem Vercel deployment...
-vercel --prod --yes
+echo yes | vercel --prod
 cd ..
 
 echo.
